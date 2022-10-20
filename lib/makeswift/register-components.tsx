447e537154c532ebcaedefe35fe11f8d2ca4d35f
Yassin <yasnaruoa@gmail.com>
2022-10-20 15:18:02 +0300

    Adding my own component

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -1,16 +1,37 @@
-import { Style } from '@makeswift/runtime/controls'
-import { ReactRuntime } from '@makeswift/runtime/react'
+import {
+  Style,
+  Shape,
+  TextInput,
+  Color,
+  Image,
+
+} from "@makeswift/runtime/controls";
+import { ReactRuntime } from "@makeswift/runtime/react";
+import FormSheet from "../components/FormSheet/formSheet";
 
 // Register your components here!
 
-function HelloWorld(props: { className?: string }) {
-  return <p {...props}>Hello, world!</p>
-}
-
-ReactRuntime.registerComponent(HelloWorld, {
-  type: 'hello-world',
-  label: 'Hello, world!',
+ReactRuntime.registerComponent(FormSheet, {
+  type: "formsheet",
+  label: "FormSheet",
   props: {
-    className: Style({ properties: Style.All }),
+    // className: Style({ properties: Style.All }),
+    // pictureUrl: Image({ label: "Profile Picture" }),
+    // boxShadow: Shape({
+    //   label: "Box Shadow",
+    //   type: {
+    //     color: Color({
+    //       label: "Color",
+    //       defaultValue: "rgba(102, 94, 255, 0.9)",
+    //     }),
+    //     offsetX: TextInput({ label: "Offset X", defaultValue: "0px" }),
+    //     offsetY: TextInput({ label: "Offset Y", defaultValue: "0px" }),
+    //     blurRadius: TextInput({ label: "Blur Radius", defaultValue: "118px" }),
+    //     spreadRadius: TextInput({
+    //       label: "Spread Radius",
+    //       defaultValue: "45px",
+    //     }),
+    //   },
+    // }),
   },
-})
+});
