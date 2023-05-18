f00174065e0422ddfed8080caf5fd5ad294cb298
pabloo <yasnarutoa@gmail.com>
2023-05-18 15:18:14 -0700

    fixing

--- pages/api/makeswift/[...makeswift].ts
+++ pages/api/makeswift/[...makeswift].ts
@@ -1,5 +1,5 @@
 import { MakeswiftApiHandler } from '@makeswift/runtime/next'
-import '../lib/components/CalculatorView/calculatorView.css'
+import '../../../lib/components/CalculatorView/calculatorView.css'
 
 const temp_text = "d4106616-dae3-4818-b559-208caa823641"
 export default MakeswiftApiHandler(temp_text!)
