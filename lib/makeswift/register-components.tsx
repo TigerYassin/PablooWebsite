1e713025c7989eb7fad04ca943c21134fdaab487
pabloo <yasnarutoa@gmail.com>
2023-05-17 19:17:31 -0700

    updates

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -8,12 +8,38 @@ import {
 } from "@makeswift/runtime/controls";
 import { ReactRuntime } from "@makeswift/runtime/react";
 import FormSheet from "../components/FormSheet/formSheet";
+import CalculatorView from "../components/CalculatorView/calculatorView";
 
 // Register your components here!
 
 ReactRuntime.registerComponent(FormSheet, {
   type: "formsheet",
   label: "FormSheet",
+  props: {
+    // className: Style({ properties: Style.All }),
+    // pictureUrl: Image({ label: "Profile Picture" }),
+    // boxShadow: Shape({
+    //   label: "Box Shadow",
+    //   type: {
+    //     color: Color({
+    //       label: "Color",
+    //       defaultValue: "rgba(102, 94, 255, 0.9)",
+    //     }),
+    //     offsetX: TextInput({ label: "Offset X", defaultValue: "0px" }),
+    //     offsetY: TextInput({ label: "Offset Y", defaultValue: "0px" }),
+    //     blurRadius: TextInput({ label: "Blur Radius", defaultValue: "118px" }),
+    //     spreadRadius: TextInput({
+    //       label: "Spread Radius",
+    //       defaultValue: "45px",
+    //     }),
+    //   },
+    // }),
+  },
+});
+
+ReactRuntime.registerComponent(CalculatorView, {
+  type: "calculatorView",
+  label: "CalculatorView",
   props: {
     // className: Style({ properties: Style.All }),
     // pictureUrl: Image({ label: "Profile Picture" }),
